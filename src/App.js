1a19de03638be8462219a805deb36379681aa218
Rahul Raj <raj.rahul686@gmail.com>
2023-08-17 16:57:47 +0530

    aaded hero image with implementation of hero section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Hero from "./components/Hero/Hero";
 import { NavBar } from "./components/NavBar/NavBar";
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
     </>
   );
 }
